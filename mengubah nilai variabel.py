a6d9eb66e14e4ed85369159527e6fef34a186a16
MuhammadSyarifudin <72082465+MuhammadSyarifudin@users.noreply.github.com>
2020-12-09 12:09:50 +0700

    Add files via upload

---
+++ mengubah nilai variabel.py
@@ -0,0 +1,3 @@
+angka_saya = 1945
+angka_saya = 1
+print(angka_saya)
---
+++ variabel yang valid.py
@@ -0,0 +1,6 @@
+nama_bahasa = "phython"
+P3MBUAT = "Guido van Rossum"
+Tahun_dibuatnya = 1991
+contoh_perusahaaan_yang_menggunakan_bahasa_phython = "Youtube, Path, Dll."
+belajar_python_oop = True
+em4ng_s3ru = "Benar"
